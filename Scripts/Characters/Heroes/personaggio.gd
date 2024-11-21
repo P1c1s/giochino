782e23d0cf3eaf10c863ab0ca7efb6fc4fe39e9f
Michela Sicuranza <michela.sicuranza@gmail.com>
2024-11-21 22:45:55 +0100

    Aggiunta animazioni Fox
    Le animazioni più importanti del personaggio secondario, prima realizzate con AnimatedSprite2D, sono state aggiornate con il nodo AnimationPlayer. È stato aggiunto anche il nodo AnimationTree con la state machine del personaggio. Il nodo AnimatedSprite2D è ancora presente nella scena come riferimento di tutte le animazioni presenti nello Sprite Sheet del personaggio

--- Scripts/Characters/Heroes/personaggio.gd
+++ Scripts/Characters/Heroes/personaggio.gd
@@ -4,27 +4,51 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -300.0
 
+var run_speed = 80.0
+
+# attack sounds
+@onready var state_machine = $AnimationTree["parameters/playback"]
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	#Gestisce il salto
+	var current_anim = state_machine.get_current_node()
+	
+	velocity.x = Input.get_axis("ui_left", "ui_right") * run_speed
+	
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		print("SALTO")		#stampa di prova
 		velocity.y = JUMP_VELOCITY
-		$AnimatedSprite2D.play("MainAttack")
+		state_machine.travel("Jump")
+		return
 	
-	#Gestisce i movimenti
-	var direction = Input.get_axis("ui_left", "ui_right")
-	
-	if direction != 0:
-		$AnimatedSprite2D.flip_h = (direction == -1)	#flip è un booleano
-	if direction:
-		velocity.x = direction * SPEED
-		$AnimatedSprite2D.play("Run")
+	if velocity.x != 0:
+		$Sprite2D.scale.x = sign(velocity.x)
+	if velocity.length() > 0:
+		state_machine.travel("Run")
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		$AnimatedSprite2D.play("Look")
-
+		state_machine.travel("Idle")
 	move_and_slide()
+
+
+	##Gestisce il salto
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
+		#$AnimatedSprite2D.play("MainAttack")
+	#
+	##Gestisce i movimenti
+	#var direction = Input.get_axis("ui_left", "ui_right")
+	#
+	#if direction != 0:
+		#$AnimatedSprite2D.flip_h = (direction == -1)	#flip è un booleano
+	#if direction:
+		#velocity.x = direction * SPEED
+		#$AnimatedSprite2D.play("Run")
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
+		#$AnimatedSprite2D.play("Look")
+#
+	#move_and_slide()
