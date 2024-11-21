cd160423bea54df0ce50abb85430cf3572ce4fde
Michela Sicuranza <michela.sicuranza@gmail.com>
2024-11-21 22:11:48 +0100

    Aggiunta animazione jump
    L'animazione del salto è stata aggiunta nell'animationTree, ma il codice nello script per lo spostamento verticale (modificando il vettore velocity) non viene eseguito correttamente. È stata corretta la UI inserendo i comandi con immagini con la stessa trasparenza

--- Scripts/Characters/Heroes/minonna.gd
+++ Scripts/Characters/Heroes/minonna.gd
@@ -2,6 +2,9 @@ extends CharacterBody2D
 
 var run_speed = 80.0
 var attacks = ["Attack"]
+
+const JUMP_VELOCITY = 300.0
+
 # attack sounds
 @onready var state_machine = $AnimationTree["parameters/playback"]
 
@@ -15,14 +18,19 @@ func _physics_process(delta):
 		
 	velocity.x = Input.get_axis("ui_left", "ui_right") * run_speed
 	if Input.is_action_just_pressed("ui_attack"):
-		print("ATTACCO")
+		print("ATTACCO")		#stampa di prova
 		state_machine.travel(attacks.pick_random())
+		return
+	
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		print("SALTO")		#stampa di prova
+		velocity.y = JUMP_VELOCITY
+		state_machine.travel("Jump")
 		return
 	
 	if velocity.x != 0:
 		$Sprite2D.scale.x = sign(velocity.x)
 	if velocity.length() > 0:
-		print("QUALCOSA")
 		state_machine.travel("Run")
 	else:
 		state_machine.travel("Idle")
