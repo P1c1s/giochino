1aba23567ab1121c1666515a745708233178450f
Michela Sicuranza <michela.sicuranza@gmail.com>
2024-12-12 16:45:20 +0100

    Aggiunta ProgressBar vita, Coins, icona numero Coins, movimento Fox
    È stata aggiunta la dinamica del personaggio Fox che segue il personaggio principale, risolvendo dei problemi legati al salto di Fox.
    È stata aggiunta una TextureProgressBar per la vita del personaggio, ma rimane da realizzare lo script del progresso legato al danno di attacchi subiti.
    È stata aggiunta la scena Coin con relativa animazione e l'icona per il numero dei Coin raccolti con relativa Label contenente il numero. Deve essere modificato il font della Label e va realizzato lo script per l'aggiornamento del numero al raccogliere dei Coin

--- Scripts/Characters/Heroes/Fox.gd
+++ Scripts/Characters/Heroes/Fox.gd
@@ -1,58 +1,77 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
 const JUMP_VELOCITY = -300.0
 
-var run_speed = 80.0
+@export var limit = 50
+
+var run_speed = 50.0
+var gravita = 800.0
+
+var player_chase = false
+var player = null
+
+@onready var timer = $Timer
 
 # attack sounds
 @onready var state_machine = $AnimationTree["parameters/playback"]
 
+#func _physics_process(delta: float) -> void:
+	## Add the gravity.
+	#var current_anim = state_machine.get_current_node()
+	#
+	#if not is_on_floor():
+		#velocity += get_gravity() * delta
+	#else:
+		#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+			#print("SALTO")		#stampa di prova
+			#velocity.y = JUMP_VELOCITY
+			#move_and_slide()
+			#state_machine.travel("Jump")
+			#return
+	#
+	#velocity.x = Input.get_axis("ui_left", "ui_right") * run_speed
+	#
+	#
+	#if velocity.x != 0:
+		#$Sprite2D.scale.x = sign(velocity.x)
+	#if velocity.length() > 0:
+		#state_machine.travel("Run")
+	#else:
+		#state_machine.travel("Idle")
+	#move_and_slide()
+
+func _on_detection_area_body_entered(body: Node2D) -> void:
+	player = body
+	player_chase = true
+	print("Player detected")
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	var current_anim = state_machine.get_current_node()
-	
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-	else:
-		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-			print("SALTO")		#stampa di prova
-			velocity.y = JUMP_VELOCITY
-			move_and_slide()
-			state_machine.travel("Jump")
-			return
-	
-	
-	velocity.x = Input.get_axis("ui_left", "ui_right") * run_speed
-	
-	
-	
-	if velocity.x != 0:
-		$Sprite2D.scale.x = sign(velocity.x)
-	if velocity.length() > 0:
-		state_machine.travel("Run")
-	else:
-		state_machine.travel("Idle")
+	velocity += get_gravity() *delta
+	var collision = get_slide_collision_count()
 	move_and_slide()
+	
+	var flip = false
+	
+	if player_chase:
+		if abs(player.position.x - position.x) > limit:
+			position.x += (player.position.x - position.x)/run_speed
+			flip = (player.position.x < position.x)
+			$Sprite2D.flip_h = flip
+			state_machine.travel("Run")
+		if not (player.velocity.length() > 0 or abs(player.position.x - position.x) > limit):
+			state_machine.travel("Idle")
+		
+		if (position.y - player.position.y) > 50 and collision > 0: #se il personaggio sta più in alto la fox salta
+			print("Stronzo più in alto")
+			velocity.y = JUMP_VELOCITY
+			state_machine.travel("Jump")
+			move_and_slide()
+		
+		if Input.is_action_just_pressed("ui_accept"):
+			timer.start(0.8)
 
 
-	##Gestisce il salto
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-		#$AnimatedSprite2D.play("MainAttack")
-	#
-	##Gestisce i movimenti
-	#var direction = Input.get_axis("ui_left", "ui_right")
-	#
-	#if direction != 0:
-		#$AnimatedSprite2D.flip_h = (direction == -1)	#flip è un booleano
-	#if direction:
-		#velocity.x = direction * SPEED
-		#$AnimatedSprite2D.play("Run")
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-		#$AnimatedSprite2D.play("Look")
-#
-	#move_and_slide()
+func _on_timer_timeout() -> void:
+	velocity.y = JUMP_VELOCITY
+	state_machine.travel("Jump")
+	move_and_slide()
--- Scripts/Characters/Villains/Cocodaemon.gd
+++ Scripts/Characters/Villains/Cocodaemon.gd
@@ -1,17 +1,26 @@
 extends CharacterBody2D
 
+
 @export var speed = 40
 @export var limit = 0.5
+
+var gravita = 140.00
 
 var startPosition
 var endPosition
 
 func _ready():
+	
+	#l'aggiunta della gravità non funziona, probabilmente per l'assegnazion
+	#startPosition = position, in cui fissiamo anche l'ordinata
+	#if not is_on_floor():
+		#velocity += get_gravity() * gravita
+	#move_and_slide()
+	#
+	
 	startPosition = position
 	endPosition = startPosition - Vector2(50, 0)
 	$Sprite2D.flip_h = true
-	if not is_on_floor():
-		velocity += get_gravity()
 
 func changeDirection():
 	var tempEnd = endPosition
@@ -26,8 +35,28 @@ func updateVelocity():
 	
 	velocity = moveDirection.normalized()*speed
 
-func _physics_process(delta):
-	
+func _physics_process(delta: float) -> void:	
 	updateVelocity()
 	move_and_slide()
-	
+
+
+
+
+	##Gestisce il salto
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
+		#$AnimatedSprite2D.play("MainAttack")
+	#
+	##Gestisce i movimenti
+	#var direction = Input.get_axis("ui_left", "ui_right")
+	#
+	#if direction != 0:
+		#$AnimatedSprite2D.flip_h = (direction == -1)	#flip è un booleano
+	#if direction:
+		#velocity.x = direction * SPEED
+		#$AnimatedSprite2D.play("Run")
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
+		#$AnimatedSprite2D.play("Look")
+#
+	#move_and_slide()
