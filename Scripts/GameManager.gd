5952380632606fcea03e8d837022499f0717f464
Michela Sicuranza <michela.sicuranza@gmail.com>
2025-01-31 16:05:22 +0100

    Aggiunta suono monete e forzieri
    Aggiunti i suoni di monete e forzieri

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -1,6 +1,10 @@
 # This script is an autoload, that can be accessed from any other script!
 extends Node2D
 
+var sound = load("res://Assets/Music/Coin.mp3")
+var audio_player = AudioStreamPlayer.new()
+
+
 var score : int = 0
 var adventurerLife : int = 100
 
@@ -9,9 +13,15 @@ var keyCollected : bool
 # Adds 1 to score variable
 func addOneScore():
 	score += 1
+	add_child(audio_player)
+	audio_player.stream = sound
+	audio_player.play()
 
 func addChestScore():
 	score += 20
+	add_child(audio_player)
+	audio_player.stream = sound
+	audio_player.play()
 
 func getDamage():
 	adventurerLife -= 10
